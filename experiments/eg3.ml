bbcea012c45b543f3c7409458bd056e5c04f3ef0
popl17 <gowtham@purdue.edu>
2017-01-25 18:45:52 -0500

    Edits.

--- experiments/eg3.ml
+++ experiments/eg3.ml
@@ -17,18 +17,22 @@ end
 
 module type MLIST = functor(A:MERGEABLE) -> sig
   include MERGEABLE
-  val empty : t Lwt.t
+  (*val empty : t Lwt.t
   val cons: A.t -> t -> t Lwt.t
-  val append: t -> t -> t Lwt.t
+  val append: t -> t -> t Lwt.t*)
   val print: t -> unit Lwt.t
+  val of_list: A.t list -> t Lwt.t
+  val to_list: t -> A.t list Lwt.t
 end
 
-module type MTREE = functor(A:MERGEABLE) -> sig
+module type MPAIR = functor(A:MERGEABLE) -> sig
   include MERGEABLE
-  val empty : t Lwt.t
+  (*val empty : t Lwt.t
   val insert: A.t -> t -> t Lwt.t
-  val coalesce: t -> t -> t Lwt.t
+  val coalesce: t -> t -> t Lwt.t*)
   val print: t -> unit Lwt.t
+  val of_pair : A.t*A.t -> t Lwt.t
+  val to_pair: t -> (A.t*A.t) Lwt.t
 end
 
 module MList: MLIST = functor(A : MERGEABLE) -> struct
@@ -97,10 +101,22 @@ module MList: MLIST = functor(A : MERGEABLE) -> struct
     AO_store.create () >>= fun ao_store ->
     AO_store.add ao_store Nil
 
-  let cons x xs_key = 
-    let new_list = Cons (x,xs_key) in
+  let cons x xs = 
+    let new_list = Cons (x,xs) in
       AO_store.create () >>= fun ao_store ->
       AO_store.add ao_store new_list 
+
+  let rec of_list = function 
+    | [] -> empty
+    | x::xs -> of_list xs >>= fun xs_key -> cons x xs_key 
+
+  let rec to_list l_key = 
+    AO_store.create () >>= fun ao_store ->
+    AO_store.read ao_store l_key >>= fun l ->
+    match from_just l with
+      | Nil -> Lwt.return []
+      | Cons (x,xs_key) -> 
+          to_list xs_key >>= fun xs -> Lwt.return @@ x::xs
 
   let rec append xs_key ys_key = 
     AO_store.create () >>= fun ao_store ->
@@ -122,73 +138,71 @@ module MList: MLIST = functor(A : MERGEABLE) -> struct
             print xs_key
           end
 
-  module C (* : Irmin.Contents.S *) = struct
 
-    type t = K.t
+  type t = K.t
 
-    let equal t1 t2 = true
-    let compare = compare
-    let hash = Hashtbl.hash
+  let equal t1 t2 = true
+  let compare = compare
+  let hash = Hashtbl.hash
 
-    let to_json k = `A [`String (K.to_hum k)]
-    let of_json = function
-      | `A [`String kstr] -> K.of_hum kstr
-      | j -> Ezjsonm.parse_error j "MList_contents.C.of_json"
-    
-    let to_string t = Ezjsonm.to_string (to_json t)
-    let of_string s = of_json (Ezjsonm.from_string s)
-    let write t buf =
-      let str = to_string t in
-      let len = String.length str in
-      Cstruct.blit_from_string str 0 buf 0 len;
-      Cstruct.shift buf len
-    let read buf =
-      Mstruct.get_string buf (Mstruct.length buf)
-      |> of_string
-    let size_of t =
-      let str = to_string t in
-      String.length str
+  let to_json k = `A [`String (K.to_hum k)]
+  let of_json = function
+    | `A [`String kstr] -> K.of_hum kstr
+    | j -> Ezjsonm.parse_error j "MList_contents.C.of_json"
+  
+  let to_string t = Ezjsonm.to_string (to_json t)
+  let of_string s = of_json (Ezjsonm.from_string s)
+  let write t buf =
+    let str = to_string t in
+    let len = String.length str in
+    Cstruct.blit_from_string str 0 buf 0 len;
+    Cstruct.shift buf len
+  let read buf =
+    Mstruct.get_string buf (Mstruct.length buf)
+    |> of_string
+  let size_of t =
+    let str = to_string t in
+    String.length str
 
-    module Path = Irmin.Path.String_list
+  module Path = Irmin.Path.String_list
 
-    let merge : Path.t -> t option Irmin.Merge.t = 
-      let merge' path ~old (v1: t option) (v2: t option)  = 
-        let _ = Printf.printf "In merge\n" in
-          match (v1,v2) with
-            | (Some xs, Some ys) -> (* this is obviously incorrect. *)
-                append xs ys >>= fun l -> Lwt.return @@ `Ok (Some l)
-            | (None, Some _) -> Lwt.return @@ `Ok v2
-            | (Some _, None) -> Lwt.return @@ `Ok v1
-            | _ -> Lwt.return @@ `Ok None in
-        merge'
-  end
-
-  include C
+  let merge : Path.t -> t option Irmin.Merge.t = 
+    let merge' path ~old (v1: t option) (v2: t option)  = 
+      let _ = Printf.printf "In merge\n" in
+        match (v1,v2) with
+          | (Some xs, Some ys) -> (* this is obviously incorrect. *)
+              append xs ys >>= fun l -> Lwt.return @@ `Ok (Some l)
+          | (None, Some _) -> Lwt.return @@ `Ok v2
+          | (Some _, None) -> Lwt.return @@ `Ok v1
+          | _ -> Lwt.return @@ `Ok None in
+      merge'
 
 end
 
 (*
  * For now, we are defining a tree to be same as a list.
  *)
-module MTree:MTREE = functor(A: MERGEABLE) -> struct
+module MPair:MPAIR = functor(A: MERGEABLE) -> struct
   module MListA = MList(A)
   include MListA
-  let insert = cons
-  let coalesce = append
+  let of_pair (a,b) = of_list [a;b]
+  let to_pair k = to_list k >>= fun l ->
+    match l with | [a;b] -> Lwt.return (a,b)
+      | _ -> failwith "Impossible"
 end
 
 module BC_store(A: MERGEABLE) = struct
   module MListA = MList(A)
-  module MTreeA = MTree(A)
+  module MPairA = MPair(A)
   type c_t = 
       | T1 of MListA.t 
-      | T2 of MTreeA.t
+      | T2 of MPairA.t
   module C = struct
     type t = c_t
 
     let equal x y = match (x,y) with
       | (T1 l1, T1 l2) -> MListA.equal l1 l2
-      | (T2 t1, T2 t2) -> MTreeA.equal t1 t2
+      | (T2 t1, T2 t2) -> MPairA.equal t1 t2
       | _ -> false
 
     let compare = compare
@@ -196,10 +210,10 @@ module BC_store(A: MERGEABLE) = struct
 
     let to_json = let open Ezjsonm in function
       | T1 l -> `A [`String "T1"; MListA.to_json l]
-      | T2 t -> `A [`String "T2"; MTreeA.to_json t]
+      | T2 t -> `A [`String "T2"; MPairA.to_json t]
     let of_json = let open Ezjsonm in function
       | `A [`String "T1"; v] -> T1 (MListA.of_json v)
-      | `A [`String "T2"; v] -> T2 (MTreeA.of_json v)
+      | `A [`String "T2"; v] -> T2 (MPairA.of_json v)
       | j -> Ezjsonm.parse_error j "MList_contents.C.of_json"
     
     let to_string t = Ezjsonm.to_string (to_json t)
@@ -232,7 +246,7 @@ module BC_store(A: MERGEABLE) = struct
               | `Conflict s -> `Conflict s
               | _ -> failwith "Impossible" in
               Lwt.return x) in
-        let (old_t : unit -> MTreeA.t option option Irmin.Merge.result Lwt.t) = 
+        let (old_t : unit -> MPairA.t option option Irmin.Merge.result Lwt.t) = 
           fun () ->
             (old () >>= fun old -> 
              let x = match old with 
@@ -250,7 +264,7 @@ module BC_store(A: MERGEABLE) = struct
                   | `Ok None -> Lwt.return @@ `Ok None
                   | `Conflict s -> Lwt.return @@ `Conflict s) 
             | (Some (T2 xs), Some (T2 ys)) -> 
-                MTreeA.merge path old_t (Some xs) (Some ys) >>= (fun res ->
+                MPairA.merge path old_t (Some xs) (Some ys) >>= (fun res ->
                 match res with
                   | `Ok (Some l) -> Lwt.return @@ `Ok (Some (T2 l))
                   | `Ok None -> Lwt.return @@ `Ok None
@@ -289,62 +303,107 @@ module Make(A: MERGEABLE) = struct
   type branch = BC_store.branch
 
   (*
-   * Lightweight branching thread monad
+   * Lightweight branch monad
    *)
-  module Lwbt = struct
-    type 'a t = branch -> 'a Lwt.t
+  module Lwb = struct
+    type named_branch = {name: string; branch: branch}
+    type st = {master  : named_branch;
+               parent  : named_branch;
+               current : named_branch}
+
+    type 'a t = st -> ('a * st) Lwt.t
+
     let return (x : 'a) : 'a t = 
-      fun br -> Lwt.return x
+      fun st -> Lwt.return (x,st)
+
     let bind (m1: 'a t) (f: 'a -> 'b t) : 'b t = 
-      fun br -> (m1 br >>= fun a -> f a br)
-    let master_do (f: 'a t) :'a Lwt.t =
-      BC_store.init () >>= fun repo -> 
-      BC_store.master repo >>= fun m -> f m
-    let clone_do name (f: 'a t) : 'a t = fun br ->
-      BC_store.clone_force br name >>= fun new_br ->
-      f new_br
-    let commit () : 'a t = fun br ->
-      BC_store.init () >>= fun repo -> 
-      BC_store.master repo >>= fun master -> 
-      BC_store.merge br ~into:master
-    let pull () : 'a t = fun br ->
-      BC_store.init () >>= fun repo -> 
-      BC_store.master repo >>= fun master -> 
-      BC_store.merge master ~into:br
+      fun st -> (m1 st >>= fun (a,st') -> f a st')
+
+    let master_st : st  = 
+      Lwt_main.run (
+        BC_store.init () >>= fun repo -> 
+        BC_store.master repo >>= fun m -> 
+        let n_m = {name="master"; branch=m} in
+          Lwt.return {master=n_m; parent=n_m; current=n_m})
+
+    let clone_st name (st: st) : st = 
+      Lwt_main.run (
+        BC_store.clone_force st.current.branch name >>= fun new_br ->
+        Lwt.return {st with parent=st.current; 
+                            current={name=name; branch=new_br}})
+
+    let commit () : unit t = fun st ->
+      let _ = Printf.printf "parent:%s, current:%s\n"
+                st.parent.name st.current.name in
+      if st.parent.name = st.current.name 
+      then Lwt.return ((),st)
+      else 
+        begin
+          BC_store.merge st.current.branch 
+            ~into:st.parent.branch >>= fun () ->
+          BC_store.clone_force st.parent.branch
+            st.current.name >>= fun new_br ->
+          Lwt.return ((), {st with current = {st.current with branch=new_br}})
+        end
+
+          (*
+    let pull () : unit t = fun st ->
+      BC_store.merge st.master.branch 
+        ~into:st.current.branch >>= fun () ->
+      Lwt.return ((),st)
+
+    let atomically_do (m : 'a t) : 'a t = fun st ->
+      m st >>= fun (a,st') ->
+      BC_store.merge st.current.branch 
+        ~into:st.parent.branch >>= fun () ->
+      BC_store.clone_force st.parent.branch
+        st.current.name >>= fun new_br ->
+      Lwt.return (a, {st' with current = {st'.current with branch=new_br}})
+           *)
+
+    let run_st (st: st) (f: 'a t): 'a = 
+      fst @@ Lwt_main.run @@ f st 
+
+    let id (f : branch -> 'a Lwt.t) : 'a t = fun st ->
+      f st.current.branch >>= fun a ->
+      Lwt.return (a,st)
+
     let (>>=) = bind
   end
 
   module type VERSIONED = sig
-    module M: MERGEABLE
+    type o
     type t
-    val new_of: M.t -> t Lwbt.t
-    val to_m: t -> M.t
-    val try_set_latest: t -> M.t -> unit Lwbt.t
-    val get_latest: t -> t Lwbt.t
+    val add_new: o -> t Lwb.t
+    val to_o: t -> o  (* IntVList.t -> int list *)
+    val mark_later: t -> o -> unit Lwb.t
+    val get_latest: t -> t Lwb.t
   end
 
-  module VList: VERSIONED with module M = MList(A) = struct
+  module VList: VERSIONED with type o = A.t list = struct
     module M = MList(A)
-
+    type o = A.t list
     type t = path * M.t
     type branch = BC_store.branch
 
     (*
      * Monadic functions
      *)
-    let new_of l_key = fun branch ->
+    let add_new l = Lwb.id @@ fun branch ->
       let bc_store = branch "creating a handle" in
       let new_path = let open Uuidm in [to_string @@ v `V4] in
+        M.of_list l >>= fun l_key ->
         BC_store.update bc_store new_path (BC_store.T1 l_key) >>= fun () ->
         Lwt.return (new_path, l_key)
 
-    let to_m (_,m) = m
+    let to_o (_,k) = Lwt_main.run @@ M.to_list k
 
-    let try_set_latest (path,_) l_key branch = 
+    let mark_later (path,_) l = Lwb.id @@ fun branch ->
       let bc_store = branch "updating a handle" in
+        M.of_list l >>= fun l_key ->
         BC_store.update bc_store path (BC_store.T1 l_key)
 
-    let get_latest (path,_) branch = 
+    let get_latest (path,_) = Lwb.id @@ fun branch ->
       let bc_store = branch "reading a handle" in
         BC_store.read bc_store path >>= fun c_t ->
         match from_just c_t with 
@@ -352,32 +411,35 @@ module Make(A: MERGEABLE) = struct
           | _ -> failwith "Unexpected"
   end
 
-  module VTree: VERSIONED with module M = MTree(A) = struct
-    module M = MTree(A)
-
+  module VPair: VERSIONED with type o = A.t * A.t = struct
+    module M = MPair(A)
+    type o = A.t * A.t
     type t = path * M.t
     type branch = BC_store.branch
 
     (*
      * Monadic functions
      *)
-    let new_of t_key branch = 
+    let add_new t = Lwb.id @@ fun branch ->
       let bc_store = branch "creating a handle" in
       let new_path = let open Uuidm in [to_string @@ v `V4] in
+        M.of_pair t >>= fun t_key ->
         BC_store.update bc_store new_path (BC_store.T2 t_key) >>= fun () ->
         Lwt.return (new_path, t_key)
 
-    let to_m (_,m) = m
+    let to_o (_,k) = Lwt_main.run @@ M.to_pair k
 
-    let try_set_latest (path,_) t_key branch = 
+    let mark_later (path,_) t = Lwb.id @@ fun branch ->
       let bc_store = branch "updating a handle" in
+        M.of_pair t >>= fun t_key ->
         BC_store.update bc_store path (BC_store.T2 t_key)
 
-    let get_latest (path,_) branch = 
+    let get_latest (path,_) = Lwb.id @@ fun branch ->
       let bc_store = branch "reading a handle" in
         BC_store.read bc_store path >>= fun c_t ->
         match from_just c_t with 
           | BC_store.T2 t_key -> Lwt.return (path, t_key)
           | _ -> failwith "Unexpected"
   end
+
 end
---
+++ experiments/eg3_main.ml
@@ -0,0 +1,42 @@
+module Eg3 = Eg3.Make(struct
+                        include Tc.Int
+                        let to_string = string_of_int
+                        let merge _ = failwith "Integers can't be merged" 
+                        module Path = Irmin.Path.String_list
+                      end)
+module IntVList = Eg3.VList
+module IntVPair = Eg3.VPair
+module Lwb = Eg3.Lwb
+
+
+(* let (run : 'a Lwt.t -> 'a) = Lwt_main.run *)
+Serializable a => a -> a -> unit
+let main x y =
+  let master = Lwb.master_st in
+  let (>>=) a b = let open Lwb in a >>= b in
+  let vl = Lwb.run_st master @@
+            let l = [1;2;3] in 
+            let x = large_in_mem_computation (l) in 
+            IntVList.add_new [5;6] in
+  let wip = Lwb.clone_st "wip" master in
+  let vl' = Lwb.run_st master
+             begin
+               let x = IntVList.to_o vl in
+               IntVList.mark_later vl (4::x) >>= fun () ->
+               Lwb.commit () >>= fun () ->
+               IntVList.get_latest vl >>= fun vl' ->
+               Lwb.return vl'
+             end in
+  let _ = Printf.printf "vl' = [%s]\n" @@ String.concat ";" @@ 
+            List.map (string_of_int) (IntVList.to_o vl') in
+  let vl'' = Lwb.run_st wip
+               begin
+                IntVList.mark_later vl [3;5;6] >>= fun () ->
+                Lwb.commit () >>= fun () ->
+                IntVList.get_latest vl
+               end in
+  let _ = Printf.printf "vl'' = [%s]\n" @@ String.concat ";" @@ 
+            List.map (string_of_int)(IntVList.to_o vl'') in
+    ();;
+
+main ();;
